780938a451e59efabd21cbe6e8e29e03e559a3bc
Manuel Vidaurre <manuel.vidaurre@agiltec.com.mx>
2014-08-04 00:58:04 -0500

    Rearranging layout

--- app/overrides/add_social_buttons_to_products.rb
+++ app/overrides/add_social_buttons_to_products.rb
@@ -1,4 +1,4 @@
 Deface::Override.new(:virtual_path => 'spree/products/show',
                      :name => 'add_social_buttons_to_products_show',
-                     :insert_after => "[data-hook='product_properties']",
+                     :insert_after => "[data-hook='product_right_part']",
                      :partial => 'spree/shared/social_buttons')
