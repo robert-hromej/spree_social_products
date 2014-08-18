a1b6dd94ae57728eb5b43b9671292a97568919e9
Manuel Vidaurre <manuel.vidaurre@agiltec.com.mx>
2014-08-17 20:44:07 -0500

    Adding report for social products shares

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Spree::Core::Engine.add_routes do
+Spree::Core::Engine.routes.append do
   namespace :admin do
     resource :social, :only => [:edit, :update], :controller => "social"
     resources :reports, :only => [] do
