438f4ff904474fd02538ea593c80144a22156078
Manuel Vidaurre <manuel.vidaurre@agiltec.com.mx>
2014-08-18 00:42:17 -0500

    Adding detailed report for Facebook

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,11 +1,21 @@
 Spree::Product.class_eval do
-  def facebook_likes(url)
+  def facebook_shares(url)
     data = open("http://graph.facebook.com/?ids=#{URI.escape(url)}").read
     data = JSON.parse(data)
     unless data == []
       data[url]['shares']
     else
       0
+    end
+  end
+
+  def facebook(url)
+    data = open("https://graph.facebook.com/fql?q=SELECT%20like_count,%20total_count,%20share_count,%20click_count,%20comment_count%20FROM%20link_stat%20WHERE%20url%20=%20%27#{URI.escape(url)}%27").read
+    data = JSON.parse(data)
+    unless data == []
+      data['data'][0]
+    else
+      {}
     end
   end
 
